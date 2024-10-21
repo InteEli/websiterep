88cd8491913a43e8a8c463e8e23fad7a20958824
Eli Sandberg Hellström <elisan14@elevad.umea.se>
2024-10-21 13:33:33 +0200

    CommitName

--- docs/_app/immutable/entry/start.BY6lsyRj.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.B31axKgp.js";export{t as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,68 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
-</div>
+<!--Delar upp sidan i två delar-->
+<main>
+  <div>
+
+    <h1 class="name"> Eli Sandberg Hellström </h1>
+    <h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
+    <h3>Dew it!<h3>
+    <!--Under denna kommer vi lägga till fler element-->
+
+	<p id="inspo">
+		"The only way to learn a new programming language is by writing programs in it." - <strong>Dennis Ritchie</strong>, creator of the C programming language.
+	</p>
+	
+	<p style="background-color:pink">
+		"Every great developer you know got there by solving problems they were unqualified to solve until they actually did it." - <span>Patrick McKenzie, software engineer and entrepreneur</span>.
+	</p>
+  </div>
+    <!--Här kommer vi lägga till en bild senare i uppgiften 1.2.4 Extra styling-->  
+  
+</main>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e7008a" fill-opacity="0.68" d="M0,96L7.3,101.3C14.5,107,29,117,44,117.3C58.2,117,73,107,87,96C101.8,85,116,75,131,58.7C145.5,43,160,21,175,53.3C189.1,85,204,171,218,213.3C232.7,256,247,256,262,250.7C276.4,245,291,235,305,229.3C320,224,335,224,349,218.7C363.6,213,378,203,393,170.7C407.3,139,422,85,436,80C450.9,75,465,117,480,165.3C494.5,213,509,267,524,245.3C538.2,224,553,128,567,80C581.8,32,596,32,611,37.3C625.5,43,640,53,655,80C669.1,107,684,149,698,170.7C712.7,192,727,192,742,160C756.4,128,771,64,785,42.7C800,21,815,43,829,69.3C843.6,96,858,128,873,160C887.3,192,902,224,916,218.7C930.9,213,945,171,960,165.3C974.5,160,989,192,1004,218.7C1018.2,245,1033,267,1047,240C1061.8,213,1076,139,1091,96C1105.5,53,1120,43,1135,58.7C1149.1,75,1164,117,1178,122.7C1192.7,128,1207,96,1222,90.7C1236.4,85,1251,107,1265,101.3C1280,96,1295,64,1309,69.3C1323.6,75,1338,117,1353,138.7C1367.3,160,1382,160,1396,138.7C1410.9,117,1425,75,1433,53.3L1440,32L1440,320L1432.7,320C1425.5,320,1411,320,1396,320C1381.8,320,1367,320,1353,320C1338.2,320,1324,320,1309,320C1294.5,320,1280,320,1265,320C1250.9,320,1236,320,1222,320C1207.3,320,1193,320,1178,320C1163.6,320,1149,320,1135,320C1120,320,1105,320,1091,320C1076.4,320,1062,320,1047,320C1032.7,320,1018,320,1004,320C989.1,320,975,320,960,320C945.5,320,931,320,916,320C901.8,320,887,320,873,320C858.2,320,844,320,829,320C814.5,320,800,320,785,320C770.9,320,756,320,742,320C727.3,320,713,320,698,320C683.6,320,669,320,655,320C640,320,625,320,611,320C596.4,320,582,320,567,320C552.7,320,538,320,524,320C509.1,320,495,320,480,320C465.5,320,451,320,436,320C421.8,320,407,320,393,320C378.2,320,364,320,349,320C334.5,320,320,320,305,320C290.9,320,276,320,262,320C247.3,320,233,320,218,320C203.6,320,189,320,175,320C160,320,145,320,131,320C116.4,320,102,320,87,320C72.7,320,58,320,44,320C29.1,320,15,320,7,320L0,320Z"></path></svg>
+<style>
+  main{
+    /*Styling för att dela upp sidan i två delar. */
+    /*Vi kommer gå igenom detta i detalj senare*/
+    margin: auto;
+    width: 80vw;
+    height: 70vh;
+    display: grid;
+    grid-template-columns: repeat(2,50%);
+    overflow: hidden;
+  }
+  div{
+    /*Styling för att centrera texten i behållaren*/
+    margin: auto;
+  }
+  
+h1{
+    color:rgb(149, 35, 35);
+}
+h2{
+    color: rgb(255, 165, 0);
+}
+h3{
+    color: #3cb371;
+    font-family: sans-serif;
+    font-weight: bold;
+}
+
+.name{
+    font-family:'Gill Sans';
+    font-size: 50px;
+}
+#inspo{
+    font-family: courier;
+    font-size: 160%;
+}
+
+span{
+    color: green;
+}
+h1, h2, h3, p {
+  display: inline-block;
+  /*Ger utrymme mellan element*/
+  margin: 10px;
+}
+  </style>
